ed9e8b6e7a16800e734ae851a1b4fe3d00937c02
Alex Ankudinov <xelez0@gmail.com>
2017-08-14 01:50:18 +0500

    Fractal size changing

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 
 import time
+import sys
 
 from matplotlib import pyplot as plt
 from fractals import make_fractal
 
+size = int(sys.argv[1]) if len(sys.argv) == 2 else 256
+
 start = time.perf_counter()
-img = make_fractal('mandelbrot', size=(256, 256), depth=64)
+img = make_fractal('mandelbrot', size=(size, size), depth=64)
 end = time.perf_counter()
 
 print('Elapsed time (in seconds): {}'.format(end - start))
