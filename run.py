be49d4383262467a1a2493f90c1945cd5e668e47
Alex Ankudinov <xelez0@gmail.com>
2017-08-14 02:58:28 +0500

    Implement argument parsing

--- run.py
+++ run.py
@@ -4,8 +4,26 @@ import time
 from matplotlib import pyplot as plt
 from fractals import make_fractal
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Short sample app')
+
+parser.add_argument('--model', default="julia")
+parser.add_argument('--size', type=int, default=256)
+parser.add_argument('--depth', type=int, default=64)
+#TODO: zoom
+#TODO: center x, y
+#TODO: some other? colormap?
+
+args = parser.parse_args()
+
+# Do main work
 start = time.perf_counter()
-img = make_fractal(size=(256, 256), depth=64)
+
+
+img = make_fractal(model=args.model,
+                   size=(args.size, args.size),
+                   depth=args.depth)
 end = time.perf_counter()
 
 print('Elapsed time (in seconds): {}'.format(end - start))
