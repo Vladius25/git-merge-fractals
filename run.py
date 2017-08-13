280dbfeac3cd29c6ffdbbce57be25ddd43de8b42
Alex Ankudinov <xelez0@gmail.com>
2017-08-14 01:41:33 +0500

    Measure time

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 
+import time
 
 from matplotlib import pyplot as plt
 from fractals import make_fractal
 
+start = time.perf_counter()
 img = make_fractal('mandelbrot', size=(256, 256), depth=64)
+end = time.perf_counter()
+
+print('Elapsed time (in seconds): {}'.format(end - start))
 plt.imshow(img, cmap="cubehelix")
 plt.show()
